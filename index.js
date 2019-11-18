5f8439d4511c0838879687f971439666a552e9c6
Sanket Goyal <48354938+Sanketgoyal26@users.noreply.github.com>
2019-11-19 01:48:49 +0530

    148191119

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.get('/home_2', isLoggedIn, function(req, res) {
-    res.render("home_2",{name: req.user.name});
+    res.render("home_2",{user: req.user});
 });
 
 
@@ -71,10 +71,78 @@ app.get('/sydney', isLoggedIn, function(req, res)
 {
     res.render('sydney')
 })
+app.get('/zurich', isLoggedIn, function(req, res)
+{
+    res.render('zurich',{user:req.user})
+})
+app.get('/shimla', isLoggedIn, function(req, res)
+{
+    res.render('shimla')
+})
+app.get('/ooty', isLoggedIn, function(req, res)
+{
+    res.render('ooty')
+})
+app.get('/munnar', isLoggedIn, function(req, res)
+{
+    res.render('munnar')
+})
+app.get('/srinagar', isLoggedIn, function(req, res)
+{
+    res.render('srinagar')
+})
+app.get('/dalhousie', isLoggedIn, function(req, res)
+{
+    res.render('dalhousie')
+})
+app.get('/andaman', isLoggedIn, function(req, res)
+{
+    res.render('andaman')
+})
+app.get('/bali', isLoggedIn, function(req, res)
+{
+    res.render('bali')
+})
+app.get('/bankok', isLoggedIn, function(req, res)
+{
+    res.render('bankok')
+})
+app.get('/goa', isLoggedIn, function(req, res)
+{
+    res.render('goa')
+})
+app.get('/hongkong', isLoggedIn, function(req, res)
+{
+    res.render('hongkong')
+})
+app.get('/levi', isLoggedIn, function(req, res)
+{
+    res.render('levi')
+})
+app.get('/maldives', isLoggedIn, function(req, res)
+{
+    res.render('maldives')
+})
+app.get('/mauritius', isLoggedIn, function(req, res)
+{
+    res.render('mauritius')
+})
+app.get('/hallstatt', isLoggedIn, function(req, res)
+{
+    res.render('hallstatt')
+})
+
+app.get('/payment', isLoggedIn, function(req, res)
+{
+    res.render('payment')
+})
+
 app.get('/login', function(req, res)
 {
     res.render('login')
 })
+
+
 
 app.post("/signup", function (req, res) {
     User.register(new User({ name : req.body.name,username:req.body.username, email: req.body.email}),req.body.password,  function (err, user) {
@@ -87,7 +155,6 @@ app.post("/signup", function (req, res) {
         });
     });
 });
-
 app.post("/login", passport.authenticate("local", {
     successRedirect: "/home_2",
     failureRedirect: "/login"
