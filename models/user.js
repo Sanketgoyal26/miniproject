8d994c9cf9b6b405adabde8cd4fbf75eb544ca63
Sanket Goyal <48354938+Sanketgoyal26@users.noreply.github.com>
2019-11-20 02:39:10 +0530

    Final

--- models/user.js
+++ models/user.js
@@ -6,6 +6,12 @@ var UserSchema = new mongoose.Schema({
     username:String,
     name:String,
     password:String,
+    country:String,
+    gender:String,
+    address:String,
+    mobile:Number,
+    dob:Date,
+    bio:String,
     email:String
 });
 
